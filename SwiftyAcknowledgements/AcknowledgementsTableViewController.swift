6cb3017165ad6c2e0d1d6c5204607077fd4a6b87
Dan Williams <danwilliams64@users.noreply.github.com>
2017-04-28 16:26:28 +0100

    Update AcknowledgementsTableViewController.swift

--- SwiftyAcknowledgements/AcknowledgementsTableViewController.swift
+++ SwiftyAcknowledgements/AcknowledgementsTableViewController.swift
@@ -15,7 +15,7 @@ open class AcknowledgementsTableViewController: UITableViewController {
     // MARK: Properties
     
     /// The text to be displayed in the **UITableView**'s **tableHeader**, if any.
-    @IBInspectable public var headerText: String? {
+    @IBInspectable open var headerText: String? {
         didSet {
             headerView.text = headerText
             updateHeaderFooterViews()
@@ -23,7 +23,7 @@ open class AcknowledgementsTableViewController: UITableViewController {
     }
     
     /// The text to be displayed in the **UITableView**'s **tableFooter**, if any.
-    @IBInspectable public var footerText: String? {
+    @IBInspectable open var footerText: String? {
         didSet {
             footerView.text = footerText
             updateHeaderFooterViews()
@@ -31,7 +31,7 @@ open class AcknowledgementsTableViewController: UITableViewController {
     }
     
     /// The font size to be used for the **UITableView**'s **tableHeader**. Defaults to the size of **UIFontTextStyleSubheadline**
-    @IBInspectable public var headerFontSize: CGFloat = UIFontDescriptor.preferredFontSize(withTextStyle: UIFontTextStyle.subheadline.rawValue) {
+    @IBInspectable open var headerFontSize: CGFloat = UIFontDescriptor.preferredFontSize(withTextStyle: UIFontTextStyle.subheadline.rawValue) {
         didSet {
             headerView.fontSize = headerFontSize
             updateHeaderFooterViews()
@@ -39,7 +39,7 @@ open class AcknowledgementsTableViewController: UITableViewController {
     }
     
     /// The font size to be used for the **UITableView**'s **tableFooter**. Defaults to the size of **UIFontTextStyleSubheadline**
-    @IBInspectable public var footerFontSize: CGFloat = UIFontDescriptor.preferredFontSize(withTextStyle: UIFontTextStyle.subheadline.rawValue) {
+    @IBInspectable open var footerFontSize: CGFloat = UIFontDescriptor.preferredFontSize(withTextStyle: UIFontTextStyle.subheadline.rawValue) {
         didSet {
             footerView.fontSize = footerFontSize
             updateHeaderFooterViews()
@@ -47,10 +47,10 @@ open class AcknowledgementsTableViewController: UITableViewController {
     }
     
     /// The font size to be used for the **UITableView**'s cells. Defaults to the size of **UIFontTextStyleBody**
-    @IBInspectable public var detailFontSize: CGFloat = UIFontDescriptor.preferredFontSize(withTextStyle: UIFontTextStyle.body.rawValue)
+    @IBInspectable open var detailFontSize: CGFloat = UIFontDescriptor.preferredFontSize(withTextStyle: UIFontTextStyle.body.rawValue)
 
     /// The name of the plist containing the acknowledgements, defaults to **Acknowledgements**.
-    @IBInspectable public var acknowledgementsPlistName = "Acknowledgements"
+    @IBInspectable open var acknowledgementsPlistName = "Acknowledgements"
     
     private lazy var _acknowledgements: [Acknowledgement] = {
         guard let
@@ -66,7 +66,7 @@ open class AcknowledgementsTableViewController: UITableViewController {
     /// The acknowledgements that are displayed by the TableViewController. The array is initialized with the contents of the
     /// plist with *acknowledgementPlistName*. To display custom acknowledements add them to the array. The tableView will
     /// reload its contents after any modification to the array.
-    public var acknowledgements: [Acknowledgement] {
+    open var acknowledgements: [Acknowledgement] {
         set {
             _acknowledgements = sortingClosure != nil ? newValue.sorted(by: sortingClosure!) : newValue
             tableView.reloadData()
@@ -80,13 +80,13 @@ open class AcknowledgementsTableViewController: UITableViewController {
     /// - Parameter lhs: acknowledgement *lhs*
     /// - Parameter rhs: acknowledgement *rhs*
     /// - Returns: A boolean indicating wether *lhs* is ordered before *rhs*
-    public typealias SortingClosure = ((Acknowledgement, Acknowledgement) -> Bool)
+    open typealias SortingClosure = ((Acknowledgement, Acknowledgement) -> Bool)
     
     /// A closure used to sort the *acknowledgements* array, defaults to a closure
     /// that sorts alphabetically. The sorting closure can be changed any time and the
     /// *acknowledgements* array will then be re-sorted and afterwards the tableView
     /// will reload its contents.
-    public var sortingClosure: SortingClosure? = { (left, right) in
+    open var sortingClosure: SortingClosure? = { (left, right) in
         var comparsion = left.title.compare(right.title)
         return comparsion == .orderedAscending
     } {
@@ -111,7 +111,7 @@ open class AcknowledgementsTableViewController: UITableViewController {
     
     // MARK: Initialization
     
-    public init(acknowledgementsPlistPath: String? = nil) {
+    open init(acknowledgementsPlistPath: String? = nil) {
         super.init(style: .grouped)
     }
     
@@ -119,13 +119,13 @@ open class AcknowledgementsTableViewController: UITableViewController {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
 
-    required public init?(coder aDecoder: NSCoder) {
+    required open init?(coder aDecoder: NSCoder) {
         super.init(style: .grouped)
     }
     
     // MARK: UIViewController overrides
     
-    override public func viewDidLoad() {
+    override open func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.reuseId)
@@ -136,7 +136,7 @@ open class AcknowledgementsTableViewController: UITableViewController {
         tableView.tableFooterView = footerView
     }
     
-    override public func viewWillAppear(_ animated: Bool) {
+    override open func viewWillAppear(_ animated: Bool) {
         if title == nil {
             title = "Acknowledgements"
         }
@@ -144,27 +144,27 @@ open class AcknowledgementsTableViewController: UITableViewController {
         super.viewWillAppear(animated)
     }
     
-    override public func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+    override open func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         updateHeaderFooterViews(forWidth: size.width)
     }
     
     // MARK: UITableViewDataSource
     
-    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override open func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.reuseId, for: indexPath)
         cell.textLabel?.text = acknowledgements[indexPath.row].title
         cell.accessoryType = .disclosureIndicator
         return cell
     }
     
-    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override open func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return acknowledgements.count
     }
     
     // MARK: UITableViewDelegate
     
-    override public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    override open func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let detailViewController = AcknowledgementViewController(acknowledgement: acknowledgements[indexPath.row])
         detailViewController.fontSize = detailFontSize
         show(detailViewController, sender: self)
